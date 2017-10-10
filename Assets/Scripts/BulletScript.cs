8f4a936da3420c0b481b4d2b919e3f9a6e120dd0
AIE\s171463 <sloth.13@hotmail.com>
2017-10-10 16:40:41 +1030

    Push for reasons?

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,8 +6,10 @@ public class BulletScript : MonoBehaviour {
     private Vector3 m_startPos;
     public float lifeDist = 100;
     public float speed = 50;
-	// Use this for initialization
-	void Start () {
+
+    public GameObject particles;
+    // Use this for initialization
+    void Start () {
         m_startPos = transform.position;
 	}
 	
@@ -19,4 +21,16 @@ public class BulletScript : MonoBehaviour {
             transform.position += transform.forward * Time.deltaTime * speed;
         }
 	}
+
+    void OnCollisionEnter(Collision c) {
+        if(c.transform.tag == "Enemy") {
+            Destroy(c.gameObject);
+
+        }
+        Instantiate<GameObject>(particles, this.transform.position, this.transform.rotation);
+        particles.GetComponent<ParticleSystem>().Emit(10);
+
+        Destroy(gameObject);
+    }
+
 }
